25fa8b6b6b5e5eeff739c9eec13d43b5cae25c7a
zummon <zummon.space@gmail.com>
2025-01-01 13:00:26 +0700

    save

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from 'svelte'
 
-  let deck = $state([
+  const deck = [
     ["♢", "A"],
     ["♢", "2"],
     ["♢", "3"],
@@ -54,59 +54,91 @@
     ["♠", "J"],
     ["♠", "Q"],
     ["♠", "K"]
-  ]);
-  let duals = $state([[], []]);
+  ]
+
+  let duals = $state([])
+	let complete = $state([])
+	let board = $state([])
 
   function shuffle () {
-    duals = duals.map((dual) => {
-      dual.forEach((card) => {
-        deck.push(card);
-      });
-      return [];
-    });
-    deck.sort(() => Math.random() - 0.5);
+		duals = [[], [], [], []]
+		complete = [[], [], [], []]
+    board = [[], [], [], [], [], [], [], []]
+    let pack = deck.toSorted(() => Math.random() - 0.5)
+		pack.forEach((card, index) => {
+			const idx = index % board.length
+			board[idx].push(card)
+		})
   }
 
+	function simulate (pick, chunkindex) {
+		if (pick[1] == 'A') {
+			let idx = 0
+			for (const slot of complete) {
+				if (!slot[0]) {
+					complete[idx] = pick
+					board[chunkindex].pop()
+					break 
+				}
+				idx++
+			}
+			return 
+		}
+		let idx = 0
+		for (const slot of duals) {
+			if (!slot[0]) {
+				duals[idx] = pick
+				board[chunkindex].pop()
+				break
+			}
+			idx++
+		}
+	}
+
   onMount(() => {
-    shuffle();
+    shuffle()
   })
 
 </script>
 
-<div class="p-4">
-  <div class="mx-auto w-fit">
-    <button class="block w-24 h-32 rounded bg-zinc-100 text-zinc-900 overflow-hidden text-4xl" onclick={() => { shuffle(); }}>
-      ⟳
-    </button>
-  </div>
-</div>
-<div class="flex flex-wrap gap-4 p-4">
-  {#each duals as dual, index}
-    <div class="bg-zinc-700 p-1">
-      <div class="font-semibold border-b">Player {index + 1}</div>
-      <div class="flex divide-x">
-        {#each dual as card}
-          <div class="px-1">
-            <div class="font-semibold">{card[1]}</div>
-            <div class="text-4xl">{card[0]}</div>
-          </div>
-        {/each}
-        <div class="">
-          <button class="text-4xl text-green-300" onclick={() => { dual.push(deck.pop()); }}>✜</button>
-        </div>
-      </div>
-    </div>
+<div class="flex flex-wrap gap-4 p-4 justify-between">
+  {#each duals as card}
+		<div class="px-1 border">
+			<div class="font-semibold">{card[1]}</div>
+			<div class="text-4xl">{card[0]}</div>
+		</div>
   {/each}
-  <div class="">
-    <button class="text-4xl text-green-300" onclick={() => { duals.push([]); }}>✜</button>
-  </div>
-</div>
-<div class="flex flex-wrap gap-4 p-4">
-  {#each deck as card}
-    <div class="px-1">
-      <div class="font-semibold">{card[1]}</div>
-      <div class="text-4xl">{card[0]}</div>
-    </div>
+	<div>
+		<button class="text-4xl text-red-400 hover:text-red-600" onclick={() => { 
+			shuffle() 
+		}}>
+			⟳
+		</button>
+	</div>
+  {#each complete as card}
+		<div class="px-1 border">
+			<div class="font-semibold">{card[1]}</div>
+			<div class="text-4xl">{card[0]}</div>
+		</div>
   {/each}
 </div>
 
+<div class="flex flex-wrap gap-4 p-4">
+  {#each board as chunk, chunkindex}
+		{@const card = chunk.at(-1)}
+		<div class="">
+			{#each chunk.slice(0, -1) as card}
+				<div class="px-1 border-t border-x cursor-default">
+					<span class="font-semibold">{card[1]}</span>
+					<span class="text-2xl">{card[0]}</span>
+				</div>
+			{/each}
+			<button class="block w-full px-1 border cursor-pointer" onclick={() => {
+				simulate(card, chunkindex)
+			}}>
+				<span class="block text-left font-semibold">{card[1]}</span>
+				<span class="block text-4xl">{card[0]}</span>
+			</button>
+		</div>
+  {/each}
+</div>
